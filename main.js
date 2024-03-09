c665035f0f3cfd9a6b0b4663d36461f9cd175e71
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-09 01:25:30 -0800

    npm and webpack setup

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Successful setup?");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi1yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJTdWNjZXNzZnVsIHNldHVwP1wiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
