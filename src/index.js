27da1d61453898b959411a58f7f1de216897caaf
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-12 14:13:43 -0700

    basic home page layout with basic styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import './styles.css';
 
 console.log("Successful setup?");
-
-function component() {
-    const element = document.createElement("div");
-    element.textContent = "Hello, world!";
-    element.classList.add("hello");
-    return element;
-}
-
-document.body.appendChild(component());
