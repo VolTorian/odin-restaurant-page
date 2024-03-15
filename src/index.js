d7ddb68173f70a001ed310edb61e30fd50a1ff41
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-15 14:22:52 -0700

    tab switching functionality added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import './styles.css';
-import {homePage} from './home.js';
+import { homePage } from './home.js';
+import { menuPage } from './menu.js';
+import { aboutPage } from './about.js';
 
-console.log("Successful setup?");
+const content = document.getElementById("content");
+const tabs = document.querySelectorAll("#nav-bar button");
+const tabNames = ["Home", "Menu", "About"];
+tabs.forEach((tabButton, index) => {
+    tabButton.addEventListener("click", (e) => {
+        loadTab(e, tabNames[index]);
+    })
+});
 
-function loadMainContent() {
-    homePage();
+function loadTab(e, tabName) {
+    content.innerHTML = "";
+    tabs.forEach((tabButton) => {tabButton.classList.remove("active")});
+    e.target.classList.add("active");
+
+    if (tabName === "Home") {
+        homePage();
+    }
+    else if (tabName === "Menu") {
+        menuPage();
+    }
+    else if (tabName === "About") {
+        aboutPage();
+    }
+    else {
+        console.log("O.o what error loading what page?");
+    }
 }
 
-loadMainContent();
+homePage();
