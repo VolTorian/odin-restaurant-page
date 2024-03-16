58584e55b5832dc07aac4a23faa6b4955a07cb0a
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-16 12:49:11 -0700

    menu page

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,28 @@
+let arbitraryItemCount = 8;
+
 function menuPage() {
+    const content = document.getElementById("content");
+
     const menu = document.createElement("div");
     menu.id = "menu-section";
     menu.classList.add("home-section");
     const menuTitle = document.createElement("div");
-    menuTitle.classList.add("home-section");
+    menuTitle.classList.add("section-title");
     menuTitle.textContent = "Menu";
+    menu.appendChild(menuTitle);
+
+    const menuContainer = document.createElement("div");
+    menuContainer.classList.add("item-container");
+    menu.appendChild(menuContainer);
+
+    for (let i = 0; i < arbitraryItemCount; i++) {
+        const menuItem = document.createElement("div");
+        menuItem.classList.add("item");
+        menuItem.textContent = "Some item on the menu";
+        menuContainer.appendChild(menuItem);
+    }
+
+    content.appendChild(menu);
 }
 
 export {menuPage};
