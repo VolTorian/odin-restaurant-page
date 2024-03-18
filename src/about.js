e1d468f3fa1bc51bc0ff0ea79f21e597e0bf3e12
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-17 20:15:36 -0700

    basic about us page

--- src/about.js
+++ src/about.js
@@ -1,10 +1,23 @@
 function aboutPage() {
+    const content = document.getElementById("content");
+
     const about = document.createElement("div");
     about.id = "about-section";
     about.classList.add("home-section");
     const aboutTitle = document.createElement("div");
-    aboutTitle.classList.add("home-section");
+    aboutTitle.classList.add("section-title");
     aboutTitle.textContent = "About";
+    about.appendChild(aboutTitle);
+
+    const aboutContent = document.createElement("div");
+    aboutContent.innerHTML = `Founded in the year 20XX, we had a humble start of serving some food.
+                                As time went by, we were able to grow our business and increase our selecttion
+                                of available choices, such as more food.
+                                <br><br>
+                                Contact us at <em>(123)456-7890</em> or <em>somerestaurant@something.com</em>`;
+    about.appendChild(aboutContent);
+
+    content.appendChild(about);
 }
 
 export {aboutPage};
